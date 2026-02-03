79865a4f019c6ef30136e0896b47955ae92bfd74
Henrique_Massas <ghrado@gmail.com>
2026-02-03 23:14:53 +0200

    Initial commit

---
+++ src/main/java/kumalizaTech/KumalizaSoftwares/KumalizaSoftwaresApplication.java
@@ -0,0 +1,13 @@
+package kumalizaTech.KumalizaSoftwares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumalizaSoftwaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumalizaSoftwaresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kumalizaTech/KumalizaSoftwares/KumalizaSoftwaresApplicationTests.java
@@ -0,0 +1,13 @@
+package kumalizaTech.KumalizaSoftwares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KumalizaSoftwaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
